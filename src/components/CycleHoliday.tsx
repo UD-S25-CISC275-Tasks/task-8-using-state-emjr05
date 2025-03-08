5e3253faca3d8d3bfc7325f5e2ee86c3e17d9c2a
Ever Merino <emjr@udel.edu>
2025-03-07 23:50:23 -0500

    only got to finish CycleHoliday and ChangeType

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,45 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+export type Holiday = "Valentine's" |"Cinco de Mayo" |"Halloween" | "Thanksgiving" | "Christmas" 
+//"🎃" | "🦃" ♥️ 🎄🪅
+
+const holidayEmojis: Record<Holiday, string> = {
+    Christmas: "🎄",
+    Halloween: "🎃",
+    "Cinco de Mayo": "🪅",
+    Thanksgiving: "🦃",
+    "Valentine's": "❤️"
+};
+
+const alphaHoliday: Record<Holiday, Holiday> = {
+    "Cinco de Mayo": "Christmas",
+    Christmas: "Halloween",
+    Halloween: "Thanksgiving",
+    Thanksgiving: "Valentine's",
+    "Valentine's": "Cinco de Mayo"
+};
+
+const dateHoliday: Record<Holiday, Holiday> = {
+    "Valentine's": "Cinco de Mayo",
+    "Cinco de Mayo" : "Thanksgiving",
+    Thanksgiving: "Halloween",
+    Halloween: "Christmas",
+    Christmas: "Valentine's"
+};
+
 export function CycleHoliday(): React.JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<Holiday>("Valentine's")
+
+
+    return <div>Cycle Holiday
+        
+        <p>Holiday: {holidayEmojis[holiday]}</p>
+        <Button onClick={() => {setHoliday(alphaHoliday[holiday])}}>
+                Advance by Alphabet
+            </Button>
+            <Button onClick={() => {setHoliday(dateHoliday[holiday])}}>
+                Advance by Year
+            </Button>
+    </div>;
 }
